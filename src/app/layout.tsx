1774d2b0bb9dbf82c3f7608d7760339fc61e71df
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-20 15:08:09 +0000

    adiciona integração do Facebook Pixel para rastreamento de eventos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const Rounded = M_PLUS_Rounded_1c({
   variable: "--font-geist-rounded",
@@ -24,6 +25,28 @@ export default function RootLayout({
       <body
         className={`${Rounded.variable} antialiased`}
       >
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1386218571513573');
+          fbq('track', 'ViewContent');
+        `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1386218571513573&ev=ViewContent&noscript=1"
+          />
+        </noscript>
         {children}
       </body>
     </html>
