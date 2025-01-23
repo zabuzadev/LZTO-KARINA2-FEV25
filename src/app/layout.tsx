1efa36c5146c226875f55b89c215ee28f89c0698
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-23 15:18:13 +0000

    adiciona dependência @next/third-parties e implementa Google Tag Manager e removido facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const Rounded = M_PLUS_Rounded_1c({
   variable: "--font-geist-rounded",
@@ -22,30 +23,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es_ES">
+      <GoogleTagManager gtmId="GTM-5CRXZ6R4" />
       <body
         className={`${Rounded.variable} antialiased`}
       >
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-        >
-          {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1386218571513573');
-          fbq('track', 'ViewContent');
-        `}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1386218571513573&ev=ViewContent&noscript=1"
-          />
+
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CRXZ6R4"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
         </noscript>
         {children}
       </body>
